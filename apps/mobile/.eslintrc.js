c6e280e037c1b743ffd8177408ec620af959515c
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-07-13 00:48:55 -0300

    chore: centralize linting rules

--- apps/mobile/.eslintrc.js
+++ apps/mobile/.eslintrc.js
@@ -1,5 +1,11 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
+  extends: [
+    'expo',
+    'plugin:prettier/recommended'
+  ],
   ignorePatterns: ['/dist/*'],
+  rules: {
+    'prettier/prettier': 'error'
+  }
 };
