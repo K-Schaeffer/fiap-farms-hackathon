1ec4908a7eb1910792ea8c60e18925f322d83085
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-07-15 20:27:39 -0300

    feat: temporary UI for dashboard

---
+++ packages/web-ui/src/components/index.tsx
@@ -0,0 +1,27 @@
+// Components
+
+import AppNavbar from './AppNavbar';
+export { AppNavbar };
+
+import Header from './Header';
+export { Header };
+
+import MainGrid from './MainGrid';
+export { MainGrid };
+
+import SideMenu from './SideMenu';
+export { SideMenu };
+
+// Shared
+// export { } from '';
+
+// Import for re-export object
+// import { } from '';
+
+// Re-export all components as a single object for easier importing
+export const Components = {
+  AppNavbar,
+  Header,
+  MainGrid,
+  SideMenu,
+};
