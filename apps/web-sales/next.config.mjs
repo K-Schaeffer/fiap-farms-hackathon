065ac26ac746a4b09cf39937c3ee28e2502b7e79
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-07-15 20:27:21 -0300

    feat: configure MUI through microfrontends

--- apps/web-sales/next.config.mjs
+++ apps/web-sales/next.config.mjs
@@ -2,6 +2,13 @@ import { NextFederationPlugin } from '@module-federation/nextjs-mf';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: [
+    '@mui/x-charts',
+    '@mui/x-data-grid',
+    '@mui/x-data-grid-pro',
+    '@mui/x-date-pickers',
+    '@mui/x-tree-view',
+  ],
   webpack: config => {
     config.output.publicPath = '/_next/';
     config.plugins.push(
--- apps/web-sales/pages/_app.tsx
+++ apps/web-sales/pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from 'next/app';
-import '../styles/global.css';
+import '@fiap-farms/web-ui/global.css';
+import { AppCacheProvider } from '@mui/material-nextjs/v14-pagesRouter';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AppCacheProvider {...pageProps}>
+      <Component {...pageProps} />
+    </AppCacheProvider>
+  );
 }
