2a0b16e06bf63acbe378b0f6f47036923217a641
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-07-15 20:27:50 -0300

    feat: set dashboard as sales app

--- apps/web-shell/pages/sales/index.tsx
+++ apps/web-shell/pages/sales/index.tsx
@@ -1,13 +1,33 @@
 import dynamic from 'next/dynamic';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import { AppNavbar } from '@fiap-farms/web-ui';
+import { Header } from '@fiap-farms/web-ui';
+import { SideMenu } from '@fiap-farms/web-ui';
 
 const SalesApp = dynamic(() => import('salesApp/Index'), {
   ssr: false,
 });
 
-export default function RootViewDashboard() {
+export default function ShellView() {
   return (
-    <>
-      <SalesApp />
-    </>
+    <Box sx={{ display: 'flex' }}>
+      <SideMenu />
+      <AppNavbar />
+      <Box component="main">
+        <Stack
+          spacing={2}
+          sx={{
+            alignItems: 'center',
+            mx: 3,
+            pb: 5,
+            mt: { xs: 8, md: 0 },
+          }}
+        >
+          <Header />
+          <SalesApp />
+        </Stack>
+      </Box>
+    </Box>
   );
 }
