dfbcd257790046841d354be1e2fe7515ece16d87
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-07-15 22:54:04 -0300

    chore: install chart and ui libs to mobile app

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,7 +1,12 @@
 import { Stack } from 'expo-router';
+import { PaperProvider } from 'react-native-paper';
 
 const AppLayout = () => {
-  return <Stack />;
+  return (
+    <PaperProvider>
+      <Stack />
+    </PaperProvider>
+  );
 };
 
 export default AppLayout;
