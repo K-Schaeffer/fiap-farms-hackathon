bbeeb5080e373e2e9100257516cdd7ab65842715
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-07-20 21:29:10 -0300

    feat: UI components for production kanban

---
+++ packages/web-ui/src/components/modals/index.ts
@@ -0,0 +1,5 @@
+export { PlantingModal } from './PlantingModal';
+export type { PlantingModalProps, PlantingFormData } from './PlantingModal';
+
+export { HarvestModal } from './HarvestModal';
+export type { HarvestModalProps, HarvestFormData } from './HarvestModal';
