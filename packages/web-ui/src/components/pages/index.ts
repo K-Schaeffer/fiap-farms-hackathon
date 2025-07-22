7291872152ed09b16fe84266ffa274650ead7718
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-07-22 20:51:20 -0300

    feat: new salel UI

--- packages/web-ui/src/components/pages/index.ts
+++ packages/web-ui/src/components/pages/index.ts
@@ -4,5 +4,11 @@ export { WebLoginPage } from './WebLoginPage';
 export { WebSignUpPage } from './WebSignUpPage';
 export { WebProductionManagement } from './WebProductionManagement';
 export { WebProductionDashboard } from './WebProductionDashboard';
+export { WebSaleForm } from './WebSaleForm';
 
 export type { WebProductionManagementProps } from './WebProductionManagement';
+export type {
+  WebSaleFormData,
+  WebSaleFormProps,
+  WebSaleProduct,
+} from './WebSaleForm';
