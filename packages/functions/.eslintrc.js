c70638e980137a6ff0f1363759dd64ac2195e545
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-07-21 21:28:18 -0300

    chore: add functions package

---
+++ packages/functions/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'google',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    sourceType: 'module',
+  },
+  ignorePatterns: [
+    '/lib/**/*', // Ignore built files.
+    '/generated/**/*', // Ignore generated files.
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'quote-props': 'off',
+    'object-curly-spacing': 'off',
+    indent: 'off',
+    'arrow-parens': 'off',
+    'max-len': 'off',
+  },
+};
